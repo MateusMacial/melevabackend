954005e6b78f60c5ac06465736448ee365e26d2c
Ezequiel Jhonatan <ezequiel-jf@hotmail.com>
2022-09-14 08:54:21 -0300

    Dtos criadas

---
+++ src/main/java/com/meleva/meleva/dto/HotelDto.java
@@ -0,0 +1,12 @@
+package com.melevabackend.melevabackend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HotelDto {
+    private String cidadeZipCode;
+    private String dataInicial;
+    private String dataFinal;
+}
